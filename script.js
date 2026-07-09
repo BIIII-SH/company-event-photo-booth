e2fb725ea337cab26a13a0b309e1d0f3b72d85f7
BIIII-SH <deszaerie.gora@google.com>
2026-07-09 14:41:40 +0800

    Milestone 2 - Setup screen and user preferences

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
 const startButton = document.getElementById("startBtn");
+const setupScreen = document.getElementById("setupScreen");
+const continueButton = document.getElementById("continueBtn");
+
 
 startButton.addEventListener("click", () => {
 
-    alert("Welcome! Settings screen will be built in Milestone 2.");
+    startButton.style.display = "none";
+
+    setupScreen.classList.remove("hidden");
+
+});
+
+
+const optionCards = document.querySelectorAll(".option-card");
+
+
+optionCards.forEach(card => {
+
+    card.addEventListener("click", () => {
+
+        const group = card.parentElement;
+
+        group.querySelectorAll(".option-card")
+        .forEach(item => {
+            item.classList.remove("selected");
+        });
+
+
+        card.classList.add("selected");
+
+    });
+
+});
+
+
+continueButton.addEventListener("click", () => {
+
+    alert("Camera setup will be added in Milestone 3!");
 
 });
